55af68dfe296c2be17971438eebd86b9a3431df8
Серов Андрей <andreyserov03@gmail.com>
2024-01-21 22:09:40 +0300

    edit servIds

--- src/database/functions/destroyUserToDatabase.ts
+++ src/database/functions/destroyUserToDatabase.ts
@@ -1,13 +1,16 @@
 import { Raiting } from "../models/Raiting";
 import { Rewards } from "../models/Rewards";
+import { Robs } from "../models/Rob";
 import { Users } from "../models/Users";
 
 export default async function destroyUserToDatabase(user: string) {
     const userDb = await Users.findOne({ where: { user_id: user } });
     const rewardsDb = await Rewards.findOne({ where: { user_id: user } });
     const raitingDb = await Raiting.findOne({ where: { user_id: user } });
+    const robDb = await Robs.findOne({ where: { user_id: user }});
 
     !userDb ? 0 : Users.destroy({ where: { user_id: user } });
     !rewardsDb ? 0 : Raiting.destroy({ where: { user_id: user } });
     !raitingDb ? 0 : Rewards.destroy({ where: { user_id: user } });
+    !robDb ? 0 : Robs.destroy({ where: { user_id: user } });    
 }
